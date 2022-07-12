258c60bbcb16a403bc3e9e0b00a5e5a40f31421f
young-oct <>
2022-07-12 17:59:54 -0300

    modified binary map creation, added image filters

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -8,13 +8,12 @@ import glob
 import numpy as np
 import cv2 as cv
 from matplotlib.patches import Circle
-from os.path import join
-import time
-from skimage import exposure
+from scipy import ndimage, misc
 
 import matplotlib.pyplot as plt
 
 from tools.auxiliary import folder_creator, arrTolist, listtoarr, load_from_oct_file
+
 
 def convert(img, target_type_min, target_type_max, target_type):
     imin = img.min()
@@ -25,13 +24,34 @@ def convert(img, target_type_min, target_type_max, target_type):
     new_img = (a * img + b).astype(target_type)
     return new_img
 
+
+def max_slice(volume):
+    # take a volume and find the index of the maximum intensity
+    # slice
+    assert volume.ndim == 3
+
+    slice = np.sum(volume, axis=0)
+    line = np.sum(slice, axis=0)
+
+    return np.argmax(line)
+
+
+def mip_stack(volume, index, thickness):
+    assert volume.ndim == 3
+
+    low_b, high_b = int(index - thickness), int(index + thickness)
+
+    if low_b >= 0 or high_b <= volume.shape[-1]:
+        return np.amax(volume[:, :, low_b::high_b], axis=2)
+
+def binary_mask(slice, vmin, vmax):
+    ret, msk = cv.threshold(slice, vmin, vmax, cv.THRESH_BINARY)
+    krn = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+
+    return cv.bitwise_and(cv.dilate(msk, krn, iterations=100), msk)
+
 if __name__ == '__main__':
 
-    # data = glob.glob('../data/distorted/*.jpeg')
-    # data = glob.glob('../data/distorted/*.oct')
-    # data = glob.glob('../data/distorted/*.png')
-    # data = glob.glob('../data/2022.07.12_1.25mm(paper)/*.oct')
-    # image = glob.glob('../data/2022.07.12_1.25mm(paper)/*.png')
     data = glob.glob('../data/2022.07.12_1mm(3dprint)/trial 2/*.oct')
     image = glob.glob('../data/2022.07.12_1mm(3dprint)/trial 2/*.png')
 
@@ -48,7 +68,7 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(1, 1, figsize=(16, 9))
 
     # # termination criteria
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
     if ret:
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
 
@@ -57,7 +77,7 @@ if __name__ == '__main__':
         corners2 = np.squeeze(corners2)
         for corner in corners2:
             coord = (int(corner[0]), int(corner[1]))
-            circ = Circle(coord, 10, edgecolor='r', fill=False, linewidth=1)
+            circ = Circle(coord, 5, edgecolor='r', fill=False, linewidth=1)
             ax.add_patch(circ)
 
     plt.title('screenshot demo', size=20)
@@ -66,60 +86,102 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
 
+    # data_decon = load_from_oct_file(data[0])
+    data_ori = load_from_oct_file(data[-1])
+
+    index = max_slice(data_ori)
+
+    # constructed maximum intensity projections from a stack with
+    # certain thickness
+    pad = 10
+    mip_slice = mip_stack(data_ori, index, pad)
+
+    med_slice = ndimage.median_filter(mip_slice, size=3)
+
+    guassian_slice = ndimage.gaussian_filter(med_slice, sigma=0.2)
+
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
-    image_sharp = cv.filter2D(src=gray, ddepth=-1, kernel=kernel)
-
-    # load .oct files deconvolved and original files
-    data_decon = load_from_oct_file(data[0])
-    data_ori = load_from_oct_file(data[-1])
-
-    # constructed maximum intensity projections of each oct volume
-    #
-    ori_mip = np.amax(data_ori, axis=2)
-    dec_mip = np.amax(data_decon, axis=2)
+    sharp_slice = cv.filter2D(src=guassian_slice, ddepth=-1, kernel=kernel)
 
     p_factor = 0.65
     vmin, vmax = int(255 * p_factor), 255
 
-    # vmin = 63
-    # vmax = 133
-    fig, ax = plt.subplots(2, 2, figsize=(16, 9))
-    ax[0, 0].imshow(ori_mip, 'gray', vmin=vmin, vmax=vmax)
-    ax[0, 0].set_axis_off()
-    ax[0, 0].set_title('mip of the original volume', size=20)
+    fig, ax = plt.subplots(3, 4, figsize=(16, 9))
+    ax[0,0].imshow(mip_slice, 'gray', vmin=vmin, vmax=vmax)
+    ax[0,0].set_axis_off()
+    ax[0,0].set_title('mip of the original volume', size=20)
 
     # plot histogram to find the cut-off for thresholding
-    ax[1, 0].hist(np.ravel(ori_mip), density=True)
+    ax[1, 0].hist(np.ravel(mip_slice), density=True)
 
-    ax[0, 1].imshow(dec_mip, 'gray', vmin=vmin, vmax=vmax)
-    ax[0, 1].set_title('mip of the deconvolved volume', size=20)
+    ax[2,0].imshow(binary_mask(mip_slice,vmin, vmax), 'gray', vmin=vmin, vmax=vmax)
+    ax[2,0].set_axis_off()
+    ax[2,0].set_title('binary image', size=20)
+
+    ax[0, 1].imshow(med_slice, 'gray', vmin=vmin, vmax=vmax)
+    ax[0, 1].set_title('median filter', size=20)
     ax[0, 1].set_axis_off()
 
     # plot histogram to find the cut-off for thresholding
-    ax[1, 1].hist(np.ravel(dec_mip), density=True)
+    ax[1, 1].hist(np.ravel(med_slice), density=True)
 
+    ax[2,1].imshow(binary_mask(med_slice,vmin, vmax), 'gray', vmin=vmin, vmax=vmax)
+    ax[2,1].set_axis_off()
+    ax[2,1].set_title('binary image', size=20)
+
+    ax[0, 2].imshow(guassian_slice, 'gray', vmin=vmin, vmax=vmax)
+    ax[0, 2].set_title('guassian filter', size=20)
+    ax[0, 2].set_axis_off()
+
+    # plot histogram to find the cut-off for thresholding
+    ax[1, 2].hist(np.ravel(guassian_slice), density=True)
+
+    ax[2,2].imshow(binary_mask(guassian_slice,vmin, vmax ), 'gray', vmin=vmin, vmax=vmax)
+    ax[2,2].set_axis_off()
+    ax[2,2].set_title('binary image', size=20)
+
+    ax[0, 3].imshow(sharp_slice, 'gray', vmin=vmin, vmax=vmax)
+    ax[0, 3].set_title('sharp filter', size=20)
+    ax[0, 3].set_axis_off()
+
+    # plot histogram to find the cut-off for thresholding
+    ax[1, 3].hist(np.ravel(sharp_slice), density=True)
+
+    ax[2,3].imshow(binary_mask(sharp_slice,vmin, vmax), 'gray', vmin=vmin, vmax=vmax)
+    ax[2,3].set_axis_off()
+    ax[2,3].set_title('binary image', size=20)
+
+    plt.tight_layout()
     plt.show()
 
-    # thresholding images to create binary maksk
-    ret, msk = cv.threshold(ori_mip,200,vmax,cv.THRESH_BINARY)
-    plt.imshow(msk,'gray')
+    mask = binary_mask(mip_slice,vmin, vmax )
+    mask = ndimage.median_filter(mask, size=2)
+    mask = cv.filter2D(src=ndimage.gaussian_filter(mask, sigma=4), ddepth=-1, kernel=kernel)
+
+    res = np.uint8(mask)
+    ret, corners = cv.findChessboardCorners(res, checked_board,
+                                            flags=cv.CALIB_CB_ADAPTIVE_THRESH +
+                                                  cv.CALIB_CB_FAST_CHECK +
+                                                  cv.CALIB_CB_NORMALIZE_IMAGE)
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 9))
+
+    if ret:
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+        cv.drawChessboardCorners(gray, checked_board, corners2, ret)
+
+        corners2 = np.squeeze(corners2)
+        for corner in corners2:
+            coord = (int(corner[0]), int(corner[1]))
+            circ = Circle(coord, 5, edgecolor='r', fill=True, linewidth=1, facecolor='r')
+            ax.add_patch(circ)
+
+    plt.title('OCT', size=20)
+    plt.axis('off')
+    plt.imshow(res, 'gray')
+    plt.tight_layout()
     plt.show()
-
-    # Extract chess-board
-    krn = cv.getStructuringElement(cv.MORPH_RECT, (200, 200))
-    dlt = cv.dilate(msk, krn, iterations=100)
-    res = 255 - cv.bitwise_and(dlt, msk)
-    plt.imshow(res,'gray')
-    plt.show()
-
-    # Displaying chess-board features
-    res = np.uint8(res)
-    ret, corners = cv.findChessboardCorners(res, (3,3),
-                                             flags=cv.CALIB_CB_ADAPTIVE_THRESH +
-                                                   cv.CALIB_CB_FAST_CHECK +
-                                                   cv.CALIB_CB_NORMALIZE_IMAGE)
-
     print(ret)
-

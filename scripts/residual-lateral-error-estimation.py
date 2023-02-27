3407b283d7f7d112f844c1e0b1ff53d4a565a217
JFarrell345 <josh.farrell@audiopticsmedical.com>
2023-02-27 21:34:23 +0000

    Calculate total error

--- scripts/residual-lateral-error-estimation.py
+++ scripts/residual-lateral-error-estimation.py
@@ -10,6 +10,14 @@ from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 from scipy.interpolate import griddata
 import discorpy.prep.preprocessing as prep
 from tools.plot import heatmap
+from sklearn.metrics import mean_squared_error
+import math
+
+# Axial error estimation
+from tools.proc import surface_index, frame_index, plane_fit,filter_mask
+from tools.proc_surface_fit import load_from_oct_file_reversed, surface_index_reverse, frame_index_reverse
+import pyransac3d as pyrsc
+from scipy.ndimage import gaussian_filter, median_filter
 
 def ExtractDotGrid(grid):
     # Create binary image of uncorrected grid
@@ -31,6 +39,25 @@ def ExtractDotGrid(grid):
 
     return extractedGrid
 
+def CalcDifferenceMap(uncorrected_grid, correction_grid):
+    extracted_uncorrected_grid = ExtractDotGrid(uncorrected_grid) / uncorrected_grid.shape[0]
+    extracted_correction_grid = ExtractDotGrid(correction_grid) / correction_grid.shape[0]
+
+    corrNormalized = (extracted_correction_grid)
+
+    diffNormalized = np.sqrt(
+        np.square((extracted_correction_grid[:, 0] - extracted_uncorrected_grid[:, 0])) +
+        np.square((extracted_correction_grid[:, 1] - extracted_uncorrected_grid[:, 1])))
+
+    # Interpolate grids to produce calibration maps
+    grid_y, grid_x = np.mgrid[0:1:512j, 0:1:512j]
+    map = griddata(
+        corrNormalized, diffNormalized,
+        (grid_y, grid_x), method='cubic', fill_value=0.0).astype('float32')
+
+    return map
+
+
 if __name__ == '__main__':
     uncorrected_grids = natsorted(glob.glob('../data/later distortion analysis/uncorrected grids/*.png'))
     correction_grids = natsorted(glob.glob('../data/later distortion analysis/correction grids/*.png'))
@@ -38,130 +65,277 @@ if __name__ == '__main__':
     error_list_mean = []
     error_list_std = []
 
-    for g in range(1):
-        uncorrected_grid = (io.imread(uncorrected_grids[-1], as_gray=True) * 255).astype(np.uint8)
-        correction_grid = (io.imread(correction_grids[-1], as_gray=True) * 255).astype(np.uint8)
+    uncorrected_grid = (io.imread(uncorrected_grids[-1], as_gray=True) * 255).astype(np.uint8)
+    corrected_grid = (io.imread(uncorrected_grids[0], as_gray=True) * 255).astype(np.uint8)
+    ref_grid = (io.imread(correction_grids[-1], as_gray=True) * 255).astype(np.uint8)
 
-        extracted_uncorrected_grid = ExtractDotGrid(uncorrected_grid)
-        extracted_correction_grid = ExtractDotGrid(correction_grid)
+    map_uncorrected = CalcDifferenceMap(uncorrected_grid, ref_grid)
+    map_corrected = CalcDifferenceMap(corrected_grid, ref_grid)
 
-        # plt.style.use('dark_background')
-        # fig = plt.figure(figsize=(16, 9))
-        # uncorr_ax = fig.add_subplot(121)
-        # uncorr_ax.set_xlim(0, correction_grid.shape[0])
-        # uncorr_ax.set_ylim(0, correction_grid.shape[1])
-        # uncorr_ax.set_ylim(uncorr_ax.get_ylim()[::-1])
-        # uncorr_ax.set_title('Uncorrected Dot Grid')
-        # uncorr_ax.set_xlabel('x')
-        # uncorr_ax.set_ylabel('y')
-        # uncorr_ax.scatter(extracted_uncorrected_grid[:, 0], extracted_uncorrected_grid[:, 1])
-        #
-        # corr_ax = fig.add_subplot(122)
-        # corr_ax.set_xlim(0, correction_grid.shape[0])
-        # corr_ax.set_ylim(0, correction_grid.shape[1])
-        # corr_ax.set_ylim(corr_ax.get_ylim()[::-1])
-        # corr_ax.set_title('Corrected Dot Grid')
-        # corr_ax.set_xlabel('x')
-        # corr_ax.set_ylabel('y')
-        # corr_ax.scatter(extracted_correction_grid[:, 0], extracted_correction_grid[:, 1])
+    # Error calculation
+    map_nan_uncorrected = map_uncorrected.copy()
+    map_nan_uncorrected[map_nan_uncorrected == 0] = np.nan
+    map_nan_corrected = map_corrected.copy()
+    map_nan_corrected[map_nan_corrected == 0] = np.nan
 
-        # Normalize dot grids
-        #uncorrNormalized = (extracted_uncorrected_grid - (np.amin(extracted_uncorrected_grid))) / np.ptp(extracted_uncorrected_grid)
-        #corrNormalized = (extracted_correction_grid - (np.amin(extracted_correction_grid))) / np.ptp(extracted_correction_grid)
+    # arrays without nan values
+    uncorrected_cropped = map_nan_uncorrected[~np.isnan(map_nan_uncorrected)]
+    corrected_cropped = map_nan_corrected[~np.isnan(map_nan_corrected)]
 
-        #uncorrNormalized = (extracted_uncorrected_grid) / correction_grid.shape[0]
-        #corrNormalized = (extracted_correction_grid) / correction_grid.shape[0]
+    # Statistics
+    error_list_mean.append(np.nanmean(map_nan_uncorrected))
+    error_list_std.append(np.nanstd(map_nan_uncorrected))
+    error_list_mean.append(np.nanmean(map_nan_corrected))
+    error_list_std.append(np.nanstd(map_nan_corrected))
 
-        uncorrNormalized = (extracted_uncorrected_grid - np.min(extracted_uncorrected_grid)) / np.ptp(extracted_uncorrected_grid)
-        corrNormalized = (extracted_correction_grid - np.min(extracted_correction_grid)) / np.ptp(extracted_correction_grid)
+    # RMSE corrected
+    MSE_corrected = mean_squared_error(corrected_cropped, np.zeros(len(corrected_cropped)))
+    RMSE_corrected = math.sqrt(MSE_corrected)
 
-        max_theta = 30.022
-        max_phi = 31.21
+    # RMSE uncorrected
+    MSE_uncorrected = mean_squared_error(uncorrected_cropped, np.zeros(len(uncorrected_cropped)))
+    RMSE_uncorrected = math.sqrt(MSE_uncorrected)
 
-        diffNormalized = np.sqrt(np.square((corrNormalized[:, 0] - uncorrNormalized[:, 0])*max_theta) + np.square((corrNormalized[:, 1] - uncorrNormalized[:, 1])*max_phi))
+    FOV = 30.0
+    label_size = 20
+    FOV_width = 10000 # um
+    axial_length = 13000 # um
 
-        # Interpolate grids to produce calibration maps
-        grid_y, grid_x = np.mgrid[0:1:512j, 0:1:512j]
-        map = griddata(
-            corrNormalized, uncorrNormalized,
-            (grid_y, grid_x), method='cubic', fill_value=0.0).astype('float32')
+    ####################### Plot the heatmap (uncorrected) #####################
+    fig, ax = plt.subplots(1, figsize=(16, 10))
+    im = ax.imshow(np.abs(map_uncorrected*FOV), cmap="hot")
 
-        # # Error calculation
-        # map_nan = map.copy()
-        # map_nan[map_nan == 0] = np.nan
-        #
-        # error_list_mean.append(np.nanmean(map_nan))
-        # error_list_std.append(np.nanstd(map_nan))
-        #
-        # fig = plt.figure(figsize=(16, 9))
-        # ax = fig.add_subplot()
-        # ax.set_title('Difference heat map', size=15)
-        # im, cbar = heatmap(map, ax=ax,
-        #                    cmap="hot", cbarlabel='angle variation')
-        #
-        # pic_dis = '../plots/lateral_error_map.pdf'
-        # # plt.axis('off')
-        #
-        # plt.savefig(pic_dis, dpi=600,
-        #             transparent=True,
-        #             bbox_inches='tight', pad_inches=0)
-        ######################## Plot the heatmap #####################
-        # fig, ax = plt.subplots(1, figsize=(16, 10))
-        # im = ax.imshow(np.abs(map), cmap="hot")
-        #
-        # # Clip to ROI
-        # # corners = [[21, 15], [21, 496], [477, 496], [477, 15]]
-        # # path = Path(corners)
-        # # patch = PathPatch(path, facecolor='none', edgecolor='none')
-        # #
-        # # plt.gca().add_patch(patch)
-        # # im.set_clip_path(patch)
-        #
-        # # Set colour bar axis height
-        # divider = make_axes_locatable(ax)
-        # cax = divider.append_axes("right", size="5%", pad=0)
-        #
-        # # Create colorbar
-        # cbar = ax.figure.colorbar(im, cax=cax)
-        # cbar.ax.set_ylabel('Absolute Angular Error (degrees)', rotation=-90, va="bottom", size=20)
-        # cbar.ax.tick_params(labelsize=15)
-        # cbar.ax.locator_params(nbins=6)
-        #
-        # ax.set_aspect(1)
-        # cax.set_aspect(18.8)
-        #
-        # # Turn spines off and create white grid.
-        # ax.spines[:].set_visible(False)
-        # ax.set_axis_off()
-        # fig.tight_layout()
+    # Set colour bar axis height
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
 
-        x_correction_map = map[:, :, 0].flatten(order='F')
-        y_correction_map = map[:, :, 1].flatten(order='F')
-        map_size = np.uint32(x_correction_map.size)
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel('Absolute angular error (degrees)', rotation=-90, va="bottom", size=20)
+    cbar.ax.tick_params(labelsize=label_size)
+    cbar.ax.locator_params(nbins=6)
 
-        correction_map = np.empty((2 * map_size), dtype=x_correction_map.dtype)
-        correction_map[0::2] = x_correction_map
-        correction_map[1::2] = y_correction_map
-        correction_map_size = np.uint32(correction_map.size / 2)
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+    ax.set_axis_off()
+    fig.tight_layout()
+
+    pic_dis = '../plots/uncorrected_angle_error_unscaled.pdf'
+    plt.savefig(pic_dis, dpi=600,
+                transparent=True,
+                bbox_inches='tight', pad_inches=0)
+
+    ####################### Plot the heatmap (residual error) #####################
+    fig, ax = plt.subplots(1, figsize=(16, 10))
+    im = ax.imshow(np.abs(map_corrected * FOV), cmap="hot")
+
+    # Set colour bar axis height
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel('Residual absolute angular error (degrees)', rotation=-90, va="bottom", size=20)
+    cbar.ax.tick_params(labelsize=label_size)
+    cbar.ax.locator_params(nbins=8)
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+    ax.set_axis_off()
+    fig.tight_layout()
+
+    pic_dis = '../plots/corrected_angle_error_unscaled.pdf'
+    plt.savefig(pic_dis, dpi=600,
+                transparent=True,
+                bbox_inches='tight', pad_inches=0)
+
+    ################################ Axial error ############################################
+
+    # load in surface data
+    data_sets_axial = natsorted(
+        glob.glob('../data/MEEI/2022.1.09(MEEI)/flat surface - corrected/*.oct'))
+    data_axial = load_from_oct_file_reversed(data_sets_axial[0], clean=False)
+
+    ################################# Surface fitting ####################################
+    p_factor = 0.65
+    vmin, vmax = int(p_factor * 255), 255
+    xz_mask = np.zeros_like(data_axial)
+
+    # perform points extraction in the xz direction
+    for i in range(data_axial.shape[0]):
+        xz_mask[i, :, :] = filter_mask(data_axial[i, :, :], vmin=vmin, vmax=vmax)
+
+    # Extract surface
+    xz_pts = surface_index_reverse(xz_mask, 0)
+    xz_pts_raw = xz_pts
+    xp, yp, zp = zip(*xz_pts)
+
+    # Crop out central artefact (GRIN rod)
+    xz_pts = []
+    outter_cutoff_percent = 0.75
+    outter_cutoff = outter_cutoff_percent * min(np.amax(xp), np.amax(yp)) / 2
+    cutoff = 25
+    bottom_crop_off = 5
+    x_offset = 11
+    y_offset = -5
+    for points in range(len(xz_pts_raw)):
+        radius = np.sqrt((xz_pts_raw[points][0] - data_axial.shape[0] / 2 + x_offset) ** 2 + (
+                xz_pts_raw[points][1] - data_axial.shape[1] / 2 + y_offset) ** 2)
+        if (radius > cutoff and xz_pts_raw[points][2] > bottom_crop_off):
+            xz_pts.append(xz_pts_raw[points])
+
+    # Unzip cropped points
+    xp, yp, zp = zip(*xz_pts)
+
+    ################################ Test Plots ##########################################
+    # fig = plt.figure(figsize=(16, 9))
+    # #fig.suptitle('index at %d plane' % z_mean)
+    # ax = fig.add_subplot(121)
+    # ax.set_title('slice %d from the xz direction' % idx)
+    # idx = 256
+    # xz_slc = frame_index_reverse(xz_mask, 'x', idx, 0)
+    # x, y = zip(*xz_slc)
+    # ax.plot(y, x, linewidth=5, alpha=0.8, color='r')
+    # ax.imshow(xz_mask[idx, :, :], cmap='gray', vmin=vmin, vmax=vmax)
     #
-    # Save correction maps to disk
-    output_folder = r'../CorrectionMap.bin'
-    # output_filename = join(output_folder, 'middle_ear-processed-volume-TM-only-phase-variance.bin')
-    with open(output_folder, 'wb') as f:
-        f.write(correction_map_size)
-        f.write(correction_map)
-    #
-    #
-    # correction_error = corrNormalized - uncorrNormalized
-    #
-    # max_th_error = np.amax(np.abs(correction_error[:,0]))
-    # max_ph_error = np.amax(np.abs(correction_error[:,1]))
-    #
-    # mean_th_error = np.mean(np.abs(correction_error[:, 0]))
-    # mean_ph_error = np.mean(np.abs(correction_error[:, 1]))
-    #
-    # std_th_error = np.std(np.abs(correction_error[:, 0]))
-    # std_phi_error = np.std(np.abs(correction_error[:, 1]))
-    #
-    # var_th_error = np.var(np.abs(correction_error[:, 0]))
-    # var_phi_error = np.var(np.abs(correction_error[:, 1]))
+    # ax = fig.add_subplot(122, projection='3d')
+    # ax.scatter(xp, yp, zp, s=0.1, alpha=0.1, c='r')
+    # ax.set_title('raw points cloud')
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('z')
+    # ax.set_xlim([0, data_axial.shape[0]])
+    # ax.set_ylim([0, data_axial.shape[1]])
+    # ax.set_zlim([0, data_axial.shape[2]])
+    # plt.tight_layout()
+    # plt.show()
+    ######################################################################################
+
+    # construct ideal plane
+    ideal_plane = pyrsc.Plane()
+    pts = np.asarray(xz_pts)
+
+    best_eq, best_inliers = ideal_plane.fit(pts, 0.1)
+    a, b, c, d = best_eq[0], best_eq[1], - best_eq[2], best_eq[3]
+    xx, yy = np.meshgrid(np.arange(0, data_axial.shape[1], 1), np.arange(0, data_axial.shape[1], 1))
+    z_ideal = (d - a * xx - b * yy) / c
+    z_mean = np.mean(z_ideal)
+
+    # Calculate the linear plane fit
+    l_plane = plane_fit(xz_pts, order=1).zc
+
+    # obtained the raw point difference map
+    raw_map = np.zeros((512, 512))
+    for i in range(len(xz_pts)):
+        lb, hb = z_mean * 0.5, z_mean * 1.5
+        if lb <= xz_pts[i][2] <= hb:
+            raw_map[xz_pts[i][0], xz_pts[i][1]] = int(xz_pts[i][2])
+        else:
+            pass
+
+    # Calculate the normalized difference map
+    dl_map = l_plane - raw_map
+    dl_map = np.where(np.abs(l_plane) == np.abs(dl_map), 0.0, dl_map)
+    dl_map_mean = dl_map[dl_map > 0.0].mean()
+    dl_map[dl_map == 0.0] = dl_map_mean
+    dl_map = np.abs((dl_map - dl_map_mean) / data_axial.shape[2])
+    #dl_map = (dl_map - dl_map_mean) / data_axial.shape[2]
+
+    fig, ax = plt.subplots(1, figsize=(16, 10))
+    im = ax.imshow(dl_map*13000, cmap="hot")
+
+    # Set colour bar axis height
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel('Absolute radial error (um)', rotation=-90, va="bottom", size=20)
+    cbar.ax.tick_params(labelsize=label_size)
+    cbar.ax.locator_params(nbins=6)
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+    ax.set_axis_off()
+    fig.tight_layout()
+
+    pic_dis = '../plots/dl_map.pdf'
+    plt.savefig(pic_dis, dpi=600,
+                transparent=True,
+                bbox_inches='tight', pad_inches=0)
+
+    # filter map
+    dl_map_filtered = dl_map.copy()
+    dl_map_filtered = gaussian_filter(dl_map_filtered, sigma=4)
+    dl_map_filtered = median_filter(dl_map_filtered, size=9)
+
+    # crop
+    dl_map_filtered[dl_map==0]=0
+    dl_map_filtered[(dl_map_filtered*axial_length) > 72.0] = 0.0
+
+    fig, ax = plt.subplots(1, figsize=(16, 10))
+    im = ax.imshow(dl_map_filtered*axial_length, cmap="hot")
+
+    # Set colour bar axis height
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel('Absolute radial error (um)', rotation=-90, va="bottom", size=20)
+    cbar.ax.tick_params(labelsize=label_size)
+    cbar.ax.locator_params(nbins=10)
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+    ax.set_axis_off()
+    fig.tight_layout()
+
+    pic_dis = '../plots/dl_map_filtered.pdf'
+    plt.savefig(pic_dis, dpi=600,
+                transparent=True,
+                bbox_inches='tight', pad_inches=0)
+
+    ########################## Combined heatmap ####################
+    # Crop axial error map to lateral error map
+    dl_map_cropped = dl_map.copy()
+    dl_map_cropped[map_uncorrected == 0] = 0
+    dl_map_cropped[dl_map == 0] = 0
+
+    dl_map_cropped = gaussian_filter(dl_map_cropped, sigma=4)
+    dl_map_cropped = median_filter(dl_map_cropped, size=9)
+    dl_map_cropped[dl_map==0]=0
+
+    total_error_map = np.sqrt((map_corrected*FOV_width)**2+(dl_map_cropped*axial_length)**2)
+    total_error_map[dl_map==0]=0
+
+    fig, ax = plt.subplots(1, figsize=(16, 10))
+    im = ax.imshow(total_error_map, cmap="hot")
+
+    # Set colour bar axis height
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, cax=cax)
+    cbar.ax.set_ylabel('Absolute total error (um)', rotation=-90, va="bottom", size=20)
+    cbar.ax.tick_params(labelsize=label_size)
+    cbar.ax.locator_params(nbins=8)
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+    ax.set_axis_off()
+    fig.tight_layout()
+
+    pic_dis = '../plots/total_error_map.pdf'
+    plt.savefig(pic_dis, dpi=600,
+                transparent=True,
+                bbox_inches='tight', pad_inches=0)
+
+    # Calculate RMSE of non-zero elements
+    total_non_zero = total_error_map[total_error_map != 0]
+    MSE = mean_squared_error(total_non_zero, np.zeros_like(total_non_zero))
+    RMSE = math.sqrt(MSE)
+
+    # Find the percentage of pixels less than RMSE
+    percentage_RMSE = ((total_non_zero <= 40.0).sum() / len(total_non_zero))*100
